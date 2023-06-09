0261da4502524f89c48bde920723682d221bd44d
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-06-09 22:01:14 +0300

    HW js7

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const galleryEl = document.querySelector('.gallery');
+
+
+const onCreateMarkup = galleryItems.map(({ preview, original, description })=>
+
+    ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+     />
+  </a>
+</li>
+ `
+);
+    
+galleryEl.insertAdjacentHTML('beforeend', onCreateMarkup.join(''))
+
+
+galleryEl.addEventListener('click', OnClickGalleryImg);
+
+function OnClickGalleryImg (evt) {
+  evt.preventDefault();
+  const isImgEl = !evt.target.classList.contains('gallery__image');
+  if (isImgEl) {
+  return
+  }
+
+evt.target.src=evt.target.dataset.source
+}
+
+
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()
+
