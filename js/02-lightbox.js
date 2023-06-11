ae22cde77920f3df6fac11d1b97651071303cf9f
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-06-11 11:02:10 +0300

    HW 7JS

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -18,28 +18,11 @@ const onCreateMarkup = galleryItems.map(({ preview, original, description })=>
  `
 );
 galleryEl.insertAdjacentHTML('beforeend', onCreateMarkup.join(''));
-galleryEl.addEventListener('click', (evt) => evt.preventDefault());
-galleryEl.addEventListener('click',OnClickGalleryImg)
-function OnClickGalleryImg(evt) {
-  
-    const isImgEl = !evt.target.classList.contains('gallery__image');
-    if (isImgEl) {
-        return
-    }
-   
-    const instance = basicLightbox.create(`
-    <img src="${evt.target.classList.gallery__link}" width="800" height="600">
-`)
-    instance.show()
 
-    galleryEl.addEventListener('keydown', OnCloswGalleryImg)
-    function OnCloswGalleryImg(evt) {
-        instance.close()
-    }
     let lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 250,
-    captionType: 'attr'
+    captionsData: 'title'
 });
-}
 
-console.log(galleryItems);
+
+
